5930a10e5375ad77ec4c2ae023e8445c4dcd49b5
ikhsangama <ikhsangama@gmail.com>
2017-01-18 18:02:07 +0700

    0.21 navbar edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="nav-wrapper">
+        <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="nav-wrapper">
 
@@ -35,7 +35,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="right hide-on-small-only">
+                    <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
